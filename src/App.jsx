27d3db342d7fbc35078ee8daa2da4b40201d39b7
shobhit <shobhitumrao5@gmail.com>
2023-08-23 14:46:18 +0530

    Auto-commit, message:- add-test datato local storage
      M  src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const dataFromAndroid = searchParams.get("data_from_android");
+    const dataFromWeb = searchParams.get("data_from_web");
+
+    localStorage.setItem("dataFromAndroid", dataFromAndroid);
+    localStorage.setItem("dataFromWeb", dataFromWeb);
+  }, []);
+
+  const [data, setData] = useState({
+    web: "",
+    android: "",
+  });
   const onClick = () => {};
   return (
     <div>
@@ -8,6 +23,27 @@ function App() {
       </h2>
 
       <button onClick={onClick}>Click to launch android app</button>
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          const dataWeb = localStorage.getItem("dataFromWeb");
+          const dataAndroid = localStorage.getItem("dataFromAndroid");
+          setData({
+            web: dataWeb,
+            android: dataAndroid,
+          });
+        }}
+      >
+        Show data from local storage
+      </button>
+      <p className="">Data shown below</p>
+      <p>
+        Web:&nbsp;&nbsp;<span>{data.web}</span>
+      </p>
+      <p>
+        Android:&nbsp;&nbsp;<span>{data.android}</span>
+      </p>
     </div>
   );
 }
