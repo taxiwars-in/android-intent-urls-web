d34dac47eaad6e6d16f051f61a284c1afd1504bd
shobhit <shobhitumrao5@gmail.com>
2023-08-23 15:19:52 +0530

    Auto-commit, message:- add asset link
      M  src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <div>
       <h1>Android intent link launch behaviour test app</h1>
       <h2>
-        Version-&nbsp;&nbsp;&nbsp;<span>1</span>
+        Version-&nbsp;&nbsp;&nbsp;<span>2</span>
       </h2>
 
       <button onClick={onClick}>Click to launch android app</button>
