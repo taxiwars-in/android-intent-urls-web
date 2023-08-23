7e5fa17cbdb29b2821d942d54da193236912a109
shobhit <shobhitumrao5@gmail.com>
2023-08-23 13:47:03 +0530

    Auto-commit, message:- icon, version
      A  public/icon/fire.png:Zone.Identifier
      A  public/icon/icon.png
      M  src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ function App() {
   return (
     <div>
       <h1>Android intent link launch behaviour test app</h1>
+      <h2>
+        Version-&nbsp;&nbsp;&nbsp;<span>1</span>
+      </h2>
 
       <button onClick={onClick}>Click to launch android app</button>
     </div>
